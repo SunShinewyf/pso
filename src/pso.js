43fb56a9123113cee23feb946955dcd7cbd52fd2
SunShinewyf <2632807692@qq.com>
2017-05-25 20:22:54 +0800

    add pso constructor

---
+++ src/pso.js
@@ -0,0 +1,19 @@
+/**
+ * date:2017-05-25
+ * author:恬竹
+ * desc:the constructor of pso
+ */
+(function(){
+   'use strict';
+   //定义构造函数
+   function Pso(){
+
+   }
+   //定义原型
+   Pso.prototype = {
+
+   };
+   
+   //返回这个对象
+   return Pso;
+})();
