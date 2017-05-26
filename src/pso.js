2babc49fbc7edecf4f77f5668f11d6eacc8599fd
SunShinewyf <2632807692@qq.com>
2017-05-26 15:08:18 +0800

    创建粒子构造函数

--- src/pso.js
+++ src/pso.js
@@ -4,62 +4,104 @@
  * desc:the constructor of pso
  */
 
-/**
- * @param maxGen number 最大迭代代数
- * @param popSize number 种群初始大小
- * @param vMax number 种群最大速度限制
- * @param vMin number 种群最小速度限制
- * @param dim number 维数限制
- * @param w number 权重系数
- */
 (function(){
    'use strict';
-   //定义构造函数
-   function Pso(maxGen,popSize,vMax,vMin,dim,w){
-        this.maxGen = maxGen || 300;
-        this.popSize = popSize || 40;
-        this.vMax = vMax || 0.5;
-        this.vMin = vMin || -0.5;
-        this.dim = dim || 2;
-        this.w = w || 0.9;
-        this.initPosition = new Array(popSize); //种群的初始位置
-        this.pbest = new Array(popSize);  //种群自身的最优值
-        this.gbest = new Array(popSize); //种群的全局最优值
+    /**
+     * desc:粒子的构造函数
+     * @param {*粒子的位置} position 
+     * @param {*粒子的速度} velocity
+     * @param {*粒子的一些参数} options 
+     */
+    function Particle(position,velocity,options){
+        this.position = position;
+        this.velocity = velocity;
+        this.popSize = options.popSize || 200;
+        this.bestPosition = new Array(this.popSize);
+    }
+
+    //粒子的原型设置
+    Particle.prototype = {
+		storePosition: function () {
+			this.bestPosition = this.position.slice(0);
+		},
+
+		getPosition: function () {
+			return this.position.slice(0);
+		},
+
+		getBestPosition: function () {
+			return this.bestPosition.slice(0);
+		},
+        
+        /**
+         * desc:更新粒子速度
+         */
+        updateVelocity: function(){
+
+        },
+
+        /**
+         * desc:更新粒子位置
+         */
+        updatePosition: function(){
+
+        }
+	};
     
-   }
-   //定义原型
-   Pso.prototype = {
-       storePosition: function () {
-           this.position = this.position.slice(0);
-       },
-       getPosition: function () {
-           return this.initPosition.slice(0);
-       },
-       getPosition: function(){
-            return this.gbest.slice(0);
-       },
-   };
-   
-   //初始化函数
-   Pso.init = function(){
+    /**
+     * desc:随机产生粒子
+     * @param {*粒子所在的区域限制} area 
+     * @param {*粒子的一些参数} options 
+     */
+    Particle.createOfRandom = function(area,options){
+        var position = area.map(function(interval){
+            return Math.random()*(interval.end - interval.start);
+        })
 
-   }
+        var velocity = area.map(function(interval){
+            return Math.random()*(interval.end - interval.start);
+        })
 
+       return new Particle(position,velocity,options);
+    }
 
-   //导出Pso对象
-   	if (typeof define === 'function' && define.amd) {
+    /**
+     * desc:粒子的位置构造函数
+     * @param {*开始位置} start 
+     * @param {*结束位置} end 
+     */
+    function interval(start,end){
+        this.start = start;
+        this.end = end;
+    }
+    
+    /**
+     * 粒子优化的构造函数
+     */
+    function Optimizer(){
+        
+    }
+
+    if (typeof define === 'function' && define.amd) {
 		// + with *RequireJS*
-		define('Pso', function () { return Pso; });
+		define('pso/Interval', function () { return Interval; });
+		define('pso/Particle', function () { return Particle; });
+		define('pso/Optimizer', function () { return Optimizer; });
 	} else {
+		var pso = {
+			Interval: Interval,
+			Particle: Particle,
+			Optimizer: Optimizer
+		};
 		if (typeof WorkerGlobalScope !== 'undefined' && self instanceof WorkerGlobalScope) {
 			// + from a *WebWorker*
-			self.Pso = Pso;
+			self.pso = pso;
 		} else if (typeof module !== 'undefined' && module.exports) {
 			// + in *node*
-			module.exports = Pso;
+			module.exports = pso;
 		} else {
 			// + or in a plain browser environment
-			window.Pso = Pso;
+			window.pso = pso;
 		}
 	}
 })();
